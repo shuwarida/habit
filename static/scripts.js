46496ef2604cef1d884f11faa18f5d947e75b687
islamov <islamov@gmail.com>
2019-11-30 17:08:11 +0600

    many fixes visual

--- static/scripts.js
+++ static/scripts.js
@@ -1,4 +1,4 @@
-var app = new Vue({
+new Vue({
   el: "#app",
 
   data: {
@@ -6,32 +6,31 @@ var app = new Vue({
     habits: JSON.parse(localStorage.getItem("habits")) || {},
     newHabit: "",
     habits_val: JSON.parse(localStorage.getItem("habits_val")) || {},
-    arr_x: -1,
-    arr_y: -1
+    arr_x: 0,
+    arr_y: 0
   },
 
   methods: {
-    clearLocalStorage: function() {
-      if (confirm("Подтверждаете удаление всех привычек?")) {
-        console.log(app.habits);
+    clearHabits: function() {
+      if (confirm("Подтверждаете удаление всех оценок?")) {
         localStorage.removeItem("habits");
-        localStorage.removeItem("habits_val");
-        app.habits = JSON.parse(localStorage.getItem("habits")) || {};
-        app.habits_val = JSON.parse(localStorage.getItem("habits_val")) || {};
+        this.habits = JSON.parse(localStorage.getItem("habits")) || {};
       }
+    },
+    clearHabitsVal: function() {
+      if (confirm("Подтверждаете удаление всех привычек?")) {
+        this.clearLocalStorage("habits_val");
+        this.habits_val = JSON.parse(localStorage.getItem("habits_val")) || {};
+      }
+    },
+    clearLocalStorage: function(key) {
+      localStorage.removeItem(key);
     },
     setCellClass: function(habbit, day_id) {
       var cssclass = "calendar__row__cell";
       if (typeof this.habits_val === "object" && this.habits_val !== null) {
-        if (
-          typeof this.habits_val[habbit] === "object" &&
-          this.habits_val[habbit] !== null
-        ) {
-          if (
-            typeof this.habits_val[habbit][day_id] === "string" &&
-            this.habits_val[habbit][day_id] !== null
-          ) {
-            console.log("im in");
+        if (typeof this.habits_val[habbit] === "object" && this.habits_val[habbit] !== null) {
+          if (typeof this.habits_val[habbit][day_id] === "string" && this.habits_val[habbit][day_id] !== null) {
             cssclass = cssclass + " habit_" + this.habits_val[habbit][day_id];
           }
         }
@@ -52,12 +51,12 @@ var app = new Vue({
     },
 
     remLocalStorage: function(index) {
-      if (confirm('Подтверждаете удаление "' + app.habits[index] + '"?')) {
-        Vue.delete(app.habits, index);
-        localStorage.setItem("habits", JSON.stringify(app.habits));
+      if (confirm('Подтверждаете удаление "' + this.habits[index] + '"?')) {
+        Vue.delete(this.habits, index);
+        localStorage.setItem("habits", JSON.stringify(this.habits));
 
-        Vue.delete(app.habits_val, index);
-        localStorage.setItem("habits_val", JSON.stringify(app.habits_val));
+        Vue.delete(this.habits_val, index);
+        localStorage.setItem("habits_val", JSON.stringify(this.habits_val));
       }
     },
     clickHabit: function(index, day_id) {
@@ -72,37 +71,21 @@ var app = new Vue({
       this.arr_x = index;
       this.arr_y = day_id;
     },
-    setHabitVal: function(arr_x, arr_y, val) {
+    setHabitVal: function(val) {
       if (typeof this.habits_val === "object" && this.habits_val !== null) {
-        if (
-          typeof this.habits_val[arr_x] === "object" &&
-          this.habits_val[arr_x] !== null
-        ) {
-          if (
-            typeof this.habits_val[arr_x][arr_y] === "string" &&
-            this.habits_val[arr_x][arr_y] !== null
-          ) {
-            this.habits_val[arr_x][arr_y] = val;
+        if (typeof this.habits_val[this.arr_x] === "object" && this.habits_val[this.arr_x] !== null) {
+          if (typeof this.habits_val[this.arr_x][this.arr_y] === "string" && this.habits_val[this.arr_x][this.arr_y] !== null) {
+            this.habits_val[this.arr_x][this.arr_y] = val;
           } else {
-            var newVal = {};
-            newVal[arr_y] = val;
-            this.habits_val[arr_x] = Object.assign(
-              {},
-              this.habits_val[arr_x],
-              newVal
-            );
+            this.habits_val[this.arr_x] = Object.assign({}, this.habits_val[this.arr_x], { [this.arr_y]: val });
           }
         } else {
-          var newKey = {};
-          var newVal = {};
-          newVal[arr_y] = val;
-          newKey[arr_x] = newVal;
-          this.habits_val = Object.assign({}, this.habits_val, newKey);
+          this.habits_val = Object.assign({}, this.habits_val, { [this.arr_x]: { [this.arr_y]: val } });
         }
       }
 
       localStorage.setItem("habits_val", JSON.stringify(this.habits_val));
-      var elem = document.querySelector('div[data-habit="' + arr_x + '"]');
+      var elem = document.querySelector('div[data-habit="' + this.arr_x + '"]');
       elem.style.display = "none";
     }
   },
